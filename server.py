bf657d4648feb5bdccc1cffd082971788a205607
Batulii98 <bthndokme@gmail.com>
2026-03-15 17:54:33 +0300

    server online icin guncellendi

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import urllib.parse
 import random
+import os
 
-PORT = 8000
-hedef_sayi = random.randint(1,10)
+PORT = int(os.environ.get("PORT", 8000))
+hedef_sayi = random.randint(1, 10)
 
 class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-
         global hedef_sayi
         sonuc = ""
         kazandi = "false"
@@ -16,7 +16,6 @@ class Handler(BaseHTTPRequestHandler):
         parsed_path = urllib.parse.urlparse(self.path)
 
         if parsed_path.path == "/tahmin":
-
             query = urllib.parse.parse_qs(parsed_path.query)
             tahmin = int(query.get("sayi", [0])[0])
 
@@ -29,13 +28,13 @@ class Handler(BaseHTTPRequestHandler):
             else:
                 sonuc = "Tebrikler! Doğru sayıyı buldun!"
                 kazandi = "true"
-                hedef_sayi = random.randint(1,10)
+                hedef_sayi = random.randint(1, 10)
 
         self.send_response(200)
-        self.send_header('Content-type','text/html; charset=utf-8')
+        self.send_header("Content-type", "text/html; charset=utf-8")
         self.end_headers()
 
-        with open("index.html","r",encoding="utf-8") as f:
+        with open("index.html", "r", encoding="utf-8") as f:
             html = f.read()
 
         html = html.replace("{{sonuc}}", sonuc)
@@ -43,6 +42,6 @@ class Handler(BaseHTTPRequestHandler):
 
         self.wfile.write(html.encode("utf-8"))
 
-with HTTPServer(("",PORT),Handler) as server:
-    print("Site çalışıyor: http://localhost:8000")
+with HTTPServer(("0.0.0.0", PORT), Handler) as server:
+    print(f"Site çalışıyor: http://0.0.0.0:{PORT}")
     server.serve_forever()
